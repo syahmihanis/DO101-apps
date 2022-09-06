ab2700febbdf9094bce3aefabffc7b0f675948c0
Syahmi Hanis <syahmi.h.chahpari@exxonmobil.com>
2022-09-06 08:23:51 +0800

    change output value

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!\n');
+  res.send('This is version 2 of the app\n');
 });
 
 app.listen(8080, function () {
